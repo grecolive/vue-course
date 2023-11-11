386a41c42b1dd0e02e5563a1a29c5b3ab6af590a
grecolive <48304759+grecolive@users.noreply.github.com>
2023-11-11 11:45:24 -0400

    Initial commit course VUE 3

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks'
+        }
+    }
+    })
+
+
+
